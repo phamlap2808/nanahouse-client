a4e2b891212d48cf29b54dea2a5a85d90e5330ab
LapPG <lappg1@runsystem.net>
2023-07-10 11:21:24 +0700

    setup eslint prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from 'path'
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr: true,
+  builder: 'vite',
+  vite: {
+    build: {
+      chunkSizeWarningLimit: 1000
+    },
+    define: {
+      __VUE_I18N_FULL_INSTALL__: true,
+      __VUE_I18N_LEGACY_API__: false
+    }
+  },
+  alias: {
+    components: resolve(__dirname, './components'),
+    middleware: resolve(__dirname, './middleware'),
+    pages: resolve(__dirname, './pages'),
+    layouts: resolve(__dirname, './layouts'),
+    composables: resolve(__dirname, './composables'),
+    store: resolve(__dirname, '.store'),
+    assets: resolve(__dirname, './assets'),
+    define: resolve(__dirname, './define')
+  }
 })
