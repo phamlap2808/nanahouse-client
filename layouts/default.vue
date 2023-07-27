ed8427b2328cfa93953c1d326cb3c138e7150b38
Lappg <lappg1@runsystem.net>
2023-07-27 07:05:08 +0700

    done header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+  import HeaderDefault from 'components/layouts/header-default.vue'
+</script>
 <template>
   <div class="layout-login">
+    <HeaderDefault />
     <slot />
   </div>
 </template>
